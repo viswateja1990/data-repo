45bc821d46ad9a82e6f171ad3f100552b9b47f28
viswateja1990 <viswatejareddy.s@gmail.com>
2017-10-16 13:59:45 -0400

    Initial Commit

---
+++ src/main/java/com/data/repo/datarepo/DataRepoApplication.java
@@ -0,0 +1,12 @@
+package com.data.repo.datarepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRepoApplication.class, args);
+	}
+}
